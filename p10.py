aa71e83648a19b891ece72ae80cd643c2f16a540
DESKTOP-PKK3F39\admin <yohan0355@daum.net>
2021-04-24 23:20:15 +0900

    python practice 2

---
+++ p10.py
@@ -0,0 +1,4 @@
+dic = {'name': 'Eric', 'age': '25'}    # 딕셔너리 형태
+
+
+print(dic['name'])
---
+++ pw.py
@@ -0,0 +1,9 @@
+a = {1: '파랑구름', 2: '이현준', 3: '민현준'}
+
+# print(a.keys())
+# print(a.values())
+# print(a.items())
+
+for k, v in a.items():
+    print("키는"+str(k))
+    print("벨류는"+v)
